67f71196fc361005ef8b27e659867ad48dea9d30
Joshua Boyd <joshuaboyd@Joshuas-MacBook-Air.local>
2019-04-19 22:56:59 -0400

    init status

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://boydj4-mean-app.herokuapp.com/api'
+  apiUrl: 'https://boydj4-mean-app.herokuapp.com/api'
   //apiUrl: 'http://localhost:3000/api'
 };
 
