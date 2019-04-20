912f2c56f70e565b91fa05513f5b7c234ee688a2
Joshua Boyd <joshuaboyd@Joshuas-MacBook-Air.local>
2019-04-20 18:57:16 -0400

    final commit

--- backend/middleware/file.js
+++ backend/middleware/file.js
@@ -13,7 +13,8 @@ const storage = multer.diskStorage({
     if (isValid) {
       error = null;
     }
-    cb(error, 'backend/images');
+    // backend/images for dev else
+    cb(error, '/images');
   },
   filename: (req, file, cb) => {
     const name = file.originalname
