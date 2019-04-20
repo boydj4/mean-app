be1275986791547736b165af5b5a03c20770ff82
Joshua Boyd <joshuaboyd@Joshuas-MacBook-Air.local>
2019-04-20 18:36:53 -0400

    final commit

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,18 +1,18 @@
-const Post = require("../models/post");
+const Post = require('../models/post');
 
 exports.createPost = (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");
+  const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
+    imagePath: url + '/images/' + req.file.filename,
     creator: req.userData.userId
   });
   post
     .save()
     .then(createdPost => {
       res.status(201).json({
-        message: "Post added successfully",
+        message: 'Post added successfully',
         post: {
           ...createdPost,
           id: createdPost._id
@@ -21,7 +21,7 @@ exports.createPost = (req, res, next) => {
     })
     .catch(error => {
       res.status(500).json({
-        message: "Creating a post failed!"
+        message: 'Creating a post failed!'
       });
     });
 };
@@ -29,8 +29,8 @@ exports.createPost = (req, res, next) => {
 exports.updatePost = (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
-    const url = req.protocol + "://" + req.get("host");
-    imagePath = url + "/images/" + req.file.filename;
+    const url = req.protocol + '://' + req.get('host');
+    imagePath = url + '/images/' + req.file.filename;
   }
   const post = new Post({
     _id: req.body.id,
@@ -42,9 +42,9 @@ exports.updatePost = (req, res, next) => {
   Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
     .then(result => {
       if (result.n > 0) {
-        res.status(200).json({ message: "Update successful!" });
+        res.status(200).json({ message: 'Update successful!' });
       } else {
-        res.status(401).json({ message: "Not authorized!" });
+        res.status(401).json({ message: 'Not authorized!' });
       }
     })
     .catch(error => {
@@ -69,14 +69,14 @@ exports.getPosts = (req, res, next) => {
     })
     .then(count => {
       res.status(200).json({
-        message: "Posts fetched successfully!",
+        message: 'Posts fetched successfully!',
         posts: fetchedPosts,
         maxPosts: count
       });
     })
     .catch(error => {
       res.status(500).json({
-        message: "Fetching posts failed!"
+        message: 'Fetching posts failed!'
       });
     });
 };
@@ -87,12 +87,12 @@ exports.getPost = (req, res, next) => {
       if (post) {
         res.status(200).json(post);
       } else {
-        res.status(404).json({ message: "Post not found!" });
+        res.status(404).json({ message: 'Post not found!' });
       }
     })
     .catch(error => {
       res.status(500).json({
-        message: "Fetching post failed!"
+        message: 'Fetching post failed!'
       });
     });
 };
@@ -102,14 +102,14 @@ exports.deletePost = (req, res, next) => {
     .then(result => {
       console.log(result);
       if (result.n > 0) {
-        res.status(200).json({ message: "Deletion successful!" });
+        res.status(200).json({ message: 'Deletion successful!' });
       } else {
-        res.status(401).json({ message: "Not authorized!" });
+        res.status(401).json({ message: 'Not authorized!' });
       }
     })
     .catch(error => {
       res.status(500).json({
-        message: "Deleting posts failed!"
+        message: 'Deleting posts failed!'
       });
     });
 };
